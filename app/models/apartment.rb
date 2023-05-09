817dda1df46514a115dbf9990ba34d1414395859
Charles Swaleh <charlswall@live.com>
2023-05-09 13:55:39 +0300

    Added the model validations

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -1,2 +1,5 @@
 class Apartment < ApplicationRecord
+  has_many :leases
+  has_many :tenants, through: :leases
+  validates :number, presence: true, uniqueness: true, numericality: { only_integer: true }
 end
