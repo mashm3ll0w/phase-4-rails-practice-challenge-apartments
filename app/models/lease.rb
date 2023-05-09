1066ff4e23955afab201d557a41f93d64b8bda05
Charles Swaleh <charlswall@live.com>
2023-05-09 16:40:39 +0300

    Created the routes for the lease model

--- app/models/lease.rb
+++ app/models/lease.rb
@@ -1,5 +1,5 @@
 class Lease < ApplicationRecord
   belongs_to :tenant
   belongs_to :apartment
-  validates: :rent, presence: true, numericality: { only_integer: true }
+  validates :rent, presence: true, numericality: { only_integer: true }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :leases
+  resources :leases, only: [:create, :destroy]
   resources :apartments, only: [:index, :show, :create, :update, :destroy]
   resources :tenants, only: [:index, :show, :create, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
