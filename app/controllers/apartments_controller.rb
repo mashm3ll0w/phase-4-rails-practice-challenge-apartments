dd14347c38785c1edaea6828304dc7f96f789f14
Charles Swaleh <charlswall@live.com>
2023-05-09 14:26:56 +0300

    Created the routes for the apartment model

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -1,2 +1,42 @@
 class ApartmentsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :apartment_not_found
+  rescue_from ActiveRecord::RecordInvalid, with: :invalid_parameters
+
+  def index
+    render json: Apartment.all, status: :ok
+  end
+
+  def show
+    apartment = find_apartment
+    render json: apartment, status: :ok
+  end
+
+  def update
+    apartment = find_apartment
+    apartment.update!(apartment_params)
+    render json: apartment, status: :ok
+  end
+
+  def create
+    apartment = Apartment.create!(apartment_params)
+    render json: apartment, status: :created
+  end
+
+  private
+
+  def find_apartment
+    Apartment.find(params[:id])
+  end
+
+  def apartment_params
+    params.permit(:number)
+  end
+
+  def invalid_parameters(invalid)
+    render json: {error: invalid.record.errors.full_messages}, status: :unprocessable_entity
+  end
+
+  def apartment_not_found
+    render json: {error: "Apartment not found"}, status: :not_found
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :leases
-  resources :apartments
+  resources :apartments, only: [:index, :show, :create, :update]
   resources :tenants
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
